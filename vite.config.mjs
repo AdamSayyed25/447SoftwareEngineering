b430c78ee0e248303c2222a97df708fcfe3907e5
Adam Sayyed <asayyed2@umbc.edu>
2025-10-27 13:31:19 -0400

    Prepare for GitHub Pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
-// vite.config.mjs
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/447SoftwareEngineering/', // 👈 must match your repo name
+})
